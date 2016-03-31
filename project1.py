e240873b5336549b30906d2266b9febcb0573e59
Mihail Eric <mihail911@gmail.com>
2016-03-31 16:13:40 -0700

    created scaffolding code

---
+++ project1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import collections
+
+from project_util import plot_histogram
+
+
+def strategy_1(num_b, num_runs):
+    bins = np.zeros(num_b)
+
+
+def strategy_2(num_b, num_runs):
+    pass
+
+
+def strategy_3(num_b, num_runs):
+    pass
+
+
+def strategy_4(num_b, num_runs):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
